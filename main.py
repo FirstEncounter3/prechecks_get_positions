021c57a65f7eeea19dfc6c0724c351ffafab1b4e
FirstEncounter3 <needenergy27@gmail.com>
2025-01-09 11:19:18 +0700

    preparation for mass receipt processing

--- main.py
+++ main.py
@@ -3,62 +3,82 @@ import csv
 import json
 import os
 
+from db import get_path_db, get_id
+
 
 def get_path_and_id():
-    while True:
-        db_path = input('Путь до базы > ')
-        if os.path.exists(db_path):
-            break
-        else:
-            print("Неверный путь. Пожалуйста, попробуйте снова.")
-
-    while True:
-        try:
-            record_id = int(input('ID записи > '))
-            break
-        except ValueError:
-            print("Пожалуйста, введите корректный числовой ID.")
-
+    db_path = get_path_db()
+    record_id = get_id()
     return db_path, record_id
 
 
 def get_positions(db_path, record_id):
+    list_record_ids = record_id.split(",")
+    result_list = []
+    
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT positions FROM DeferredChecks WHERE _id = ?", (record_id,))
-        positions = cursor.fetchone()
-    return json.loads(positions[0]) if positions else None
+
+        placeholders = ', '.join('?' for _ in list_record_ids)
+        query = f"SELECT positions FROM DeferredChecks WHERE _id IN ({placeholders})"
+        
+        cursor.execute(query, list_record_ids)
+        results = cursor.fetchall()
+        
+        for positions in results:
+            if positions:
+                result_list.extend(json.loads(positions[0]))
+
+    return result_list
 
 
-def prepare_positions(db_path, record_id):
-    result_object = {}
-    result_list_of_objects = []
-
+def create_object(db_path, record_id):
     original_list = get_positions(db_path, record_id)
+    result_list_of_objects = []
+    ids = record_id.split(",")
 
-    for elem in original_list:
-        result_object["Наименование"] = elem["name"]
-        result_object["Цена"] = elem["price"]
-        result_object["Количество"] = elem["quantity"]
-        result_object["Код товара"] = elem["productCode"]
-        result_object["Сумма"] = str(elem["price"] * elem["quantity"]).replace(".", ",")
-        result_list_of_objects.append(result_object)
-        result_object = {}
+    for index, elem in enumerate(original_list):
+        try:
+            result_object = {}
+            result_object["Наименование"] = elem["name"]
+            result_object["Цена"] = elem["price"]
+            result_object["Количество"] = elem["quantity"]
+            result_object["Код товара"] = elem["productCode"]
+            result_object["Штрихкод"] = f'{elem.get("barcode")}'
+            result_object["Сумма"] = str(elem["price"] * elem["quantity"]).replace(".", ",")
+            result_list_of_objects.append(result_object)
+
+        except KeyError as e:
+            print(f"Данные о позиции {elem} отсутствуют в базе данных. Ошибка: {e}")
 
     return result_list_of_objects
 
 
 def write_to_csv(db_path, record_id):
-    object_to_write = prepare_positions(db_path, record_id)
+    object_to_write = create_object(db_path, record_id)
+    ids = record_id.split(",")
+    print(object_to_write)
 
-    with open(str(record_id) + ".csv", "w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=object_to_write[0].keys())
+    if not object_to_write:
+        print("Нет данных для записи в CSV.")
+        return
 
-        writer.writeheader()
-        writer.writerows(object_to_write)
+    try:
+        with open(f"{record_id}.csv", "w", newline="", encoding="utf-8") as file:
+            fieldnames = list(object_to_write[0].keys())
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for elem in object_to_write:
+                writer.writerow(elem)
+
+    except PermissionError:
+        print("Ошибка доступа к файлу. Проверьте, что файл не открыт в другом приложении.")
+    except Exception as e:
+        print(f"Произошла ошибка при записи в CSV: {e}")
 
 
 
 if __name__ == "__main__":
     db_path, record_id = get_path_and_id()
+    # print(create_object(db_path, record_id))
     write_to_csv(db_path, record_id)
